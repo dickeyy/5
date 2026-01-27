4b58d1a7ee3e5d3734f06205e4239f4698434fb1
Kyle Dickey <k@kyle.so>
2026-01-27 16:32:24 -0700

    add guild struct

--- structs/discord.go
+++ structs/discord.go
@@ -1,8 +1,31 @@
 package structs
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 type DiscordCommand struct {
 	*discordgo.ApplicationCommand
 	Handler func(s *discordgo.Session, i *discordgo.InteractionCreate) *discordgo.InteractionResponse
+}
+
+// internal struct for guilds (includes settings, etc.)
+type Guild struct {
+	ID          string        `json:"id"`
+	Name        string        `json:"name"`
+	Description string        `json:"description"`
+	IconURL     string        `json:"icon_url"`
+	OwnerID     string        `json:"owner_id"`
+	CreatedAt   time.Time     `json:"created_at"`
+	UpdatedAt   time.Time     `json:"updated_at"`
+	DeletedAt   *time.Time    `json:"deleted_at,omitempty"`
+	Settings    GuildSettings `json:"settings"`
+}
+
+// this will change a lot
+type GuildSettings struct {
+	ID      string `json:"id"`
+	GuildID string `json:"guild_id"`
 }
