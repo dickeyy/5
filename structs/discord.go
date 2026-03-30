b38aac67e0bbb68b7623cbcdf06308a2c2f71540
Kyle Dickey <k@kyle.so>
2026-03-30 16:19:06 -0600

    add initial schema

--- structs/discord.go
+++ structs/discord.go
@@ -15,21 +15,21 @@ type DiscordEvent struct {
 	Handler any
 }
 
-// internal struct for guilds (includes settings, etc.)
-type Guild struct {
-	ID          string        `json:"id"`
-	Name        string        `json:"name"`
-	Description string        `json:"description"`
-	IconURL     string        `json:"icon_url"`
-	OwnerID     string        `json:"owner_id"`
-	CreatedAt   time.Time     `json:"created_at"`
-	UpdatedAt   time.Time     `json:"updated_at"`
-	DeletedAt   *time.Time    `json:"deleted_at,omitempty"`
-	Settings    GuildSettings `json:"settings"`
+// internal Discord snapshot (non-schema)
+type DiscordGuild struct {
+	ID          string               `json:"id"`
+	Name        string               `json:"name"`
+	Description string               `json:"description"`
+	IconURL     string               `json:"icon_url"`
+	OwnerID     string               `json:"owner_id"`
+	CreatedAt   time.Time            `json:"created_at"`
+	UpdatedAt   time.Time            `json:"updated_at"`
+	DeletedAt   *time.Time           `json:"deleted_at,omitempty"`
+	Settings    DiscordGuildSettings `json:"settings"`
 }
 
 // this will change a lot
-type GuildSettings struct {
+type DiscordGuildSettings struct {
 	ID      string `json:"id"`
 	GuildID string `json:"guild_id"`
 }
